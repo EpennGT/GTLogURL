caa1215e7edcc0da1e0c967d07ff428fcc12dff1
EpennGT <razerboy518@gmail.com>
2025-02-27 05:09:48 +0700

    Create server_data.php

---
+++ growtopia/server_data.php
@@ -0,0 +1,7 @@
+server|38.46.218.110
+port|17092
+type|1
+#maint|`5The server has on maintenance mode. Please check information about the server again.
+loginurl|skyhosting.site
+beta_server|127.0.0.1
+beta_port|17091
