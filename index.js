5bcfaa8fc4cb4c6e14945170e0bff5fcfebee2bd
EpennGT <razerboy518@gmail.com>
2025-02-27 05:15:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,11 @@ app.use(express.json());
 // Route utama
 app.get('/', (req, res) => {
     res.send('Hello, world!');
+});
+
+// Endpoint untuk growtopia/server_data.php
+app.get('/growtopia/server_data.php', (req, res) => {
+    res.sendFile(path.join(__dirname, 'growtopia', 'server_data.php'));
 });
 
 // Menjalankan server
